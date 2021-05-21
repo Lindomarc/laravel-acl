05d18da8c68f6cc1ebe5eeff286557919a043c42
lindomarc <lindomarc@outlook.com>
2021-05-20 22:51:07 -0300

    fix: add link post manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,9 @@
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
+                                    <a class="dropdown-item" href="{{ route('post.index') }}">
+                                        Posts Manager
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('user.index') }}">
                                         Users Manager
                                     </a>
